bb8e8c45323511ee3f0f17e8547a6c01cb80b4da
nikcbg <nikcbg@yahoo.com>
2021-07-13 09:11:14 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "tfe_organization" "test" {
+  name  = "nikolay-paid"
+  email = "nikolay@hashicorp.com"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "my-workspace-name"
+  organization = tfe_organization.test.id
+}
+
+
+resource "tfe_notification_configuration" "test" {
+  name             = "my-test-email-notification-configuration"
+  enabled          = true
+  destination_type = "nikolay@hashicorp.com"
+  email_user_ids   = [tfe_organization_membership.test.user_id]
+  triggers         = ["run:created", "run:planning", "run:errored"]
+  workspace_id     = tfe_workspace.test.id
+}
